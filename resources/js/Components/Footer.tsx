f312fd3a2527614ee2a60bd60b407e0a9fdb164d
Jeanikt <jeanikkt@gmail.com>
2024-07-26 12:24:47 -0300

    adiccao de footer na pagina de welcome e login

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 w-full py-4 bg-[#0A0A0A] text-white text-center">
+        <footer className="fixed bottom-0 left-0 w-full py-4 bg-[#0A0A0A] text-gray-300 text-center">
             <p>
                 &copy; {new Date().getFullYear()} MyBazar. All rights reserved.
             </p>
